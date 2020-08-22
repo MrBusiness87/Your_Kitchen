bafc174a1386639a6fd325099851acd04ec0c2e4
Pha Kpa <kphaoctober@yahoo.com>
2020-08-22 15:31:32 -0400

    added myIngredients/recipeView

---
+++ public/js/myIngredients.js
@@ -0,0 +1,7 @@
+const searchButton = $("#searchButton");
+const ingredientsDiv = $("#ingredients"); //ingredients div for display
+
+searchButton.on("click", (event) => {
+  event.preventDefault();
+  //When search button is click, this happens
+});
