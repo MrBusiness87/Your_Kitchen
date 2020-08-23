a5a4eed0cc46cb0491ed3a0f955895bd549da399
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-22 21:28:46 -0400

    updated schema to work properly

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ USE your_kitchenDB;
 
 CREATE TABLE users_ingredients (
     id INT NOT NULL AUTO_INCREMENT,
-    ingredient_id INT NOT NULL,
+    ingredient_id INT,
     ingredient_name VARCHAR(50),
-    ingredient_amount INT NOT NULL,
+    ingredient_amount INT,
     ingredient_unit VARCHAR(50),    
 );
 
 CREATE TABLE favorite_recipes(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     recipe_id INT NOT NULL,
     recipe_name VARCHAR (50),
 );
