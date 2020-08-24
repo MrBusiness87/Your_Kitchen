d9d6c1df0e9d63c61a2a224659d768f64ab321de
MrBusiness87 <bbartula87@gmail.com>
2020-08-24 18:39:41 -0400

    08/24/2020-Inserted information to get recipes to display from API.

--- public/js/recipeView.js
+++ public/js/recipeView.js
@@ -1,48 +1,20 @@
-const update = $("#update");
-const favorite = $("#favorite");
-const recipeImg = $("#recipeImg"); //recipe image div for display
-const ingredients = $("#ingredients"); //ingredients div for display
-const ingredientsNeeded = $("#ingredientsNeeded");
-
-update.on("click", () => {
-  //When update button is click, this happens
-});
-
-favorite.on("click", () => {
-  //When favorite button is click, this happens
-});
-
-
-var form = new FormData();
-form.append("author", "Emily Henderson");
-form.append("backgroundColor", "#ffffff");
-form.append("fontColor", "#333333");
-form.append("source", "spoonacular.com");
-form.append("backgroundImage", "background1");
-form.append("image", "The image.");
-form.append("ingredients", "2 cups of green beans");
-form.append("instructions", "cook the beans");
-form.append("mask", "ellipseMask");
-form.append("readyInMinutes", "60");
-form.append("servings", "2");
-form.append("title", "Pork tenderloin with green beans");
-
 var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/visualizeRecipe",
-  "method": "POST",
-  "headers": {
+  async: true,
+  crossDomain: true,
+  url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/4632/summary",
+  method: "GET",
+  headers: {
     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     "x-rapidapi-key": "0ada17a314mshe0574db4c12a595p187297jsn28f33c886f1b",
-    "content-type": "multipart/form-data"
   },
-  "processData": false,
-  "contentType": false,
-  "mimeType": "multipart/form-data",
-  "data": form
 }
 
 $.ajax(settings).done(function (response) {
   console.log(response);
+  let title = "<p>" + response.title + "</p>";
+  console.log(title);
+  $("#title").append(title);
+  let summary = "<p>" + response.summary + "</p>";
+  console.log(summary);
+  $("#summary").append(summary);
 });
