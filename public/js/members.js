9d2e9bc6321e340b9c0d19180876cbaa2404aa01
MrBusiness87 <bbartula87@gmail.com>
2020-08-25 19:18:32 -0400

    08/25/2020-The search Button click is searching for the selected meat.

--- public/js/members.js
+++ public/js/members.js
@@ -1,28 +1,36 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=apples%252Cflour%252Csugar",
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      "x-rapidapi-key": "0ada17a314mshe0574db4c12a595p187297jsn28f33c886f1b",
-    },
-  };
 
 
-
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    let recipe = "<h3>" + response[0].title + "</h3><br><img src='" + response[0].image + "'><br><h5>You will need: " + response[0].usedIngredients[0].name + "</h5>";
-    console.log(recipe);
-    $("#recipes").append(recipe);
+  var search = "";
+  $("#dropdown1").click(function (event) {
+    console.log("Button sent Info", event.target.innerHTML);
+    search = event.target.innerHTML;
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=" + search,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "0ada17a314mshe0574db4c12a595p187297jsn28f33c886f1b",
+      },
+    };
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      let recipe = "<h3>" + response[0].title + "<br><h3>Recipe #: " + response[0].id + "</h3><br><img src='" + response[0].image + "'><br><h5>You will need: " + response[0].usedIngredients[0].name + "</h5>";
+      console.log(recipe);
+      $("#recipes").append(recipe);
+    });
   });
+
+
+
 
   // $.get("/api/user_data").then(data => {
   //   $(".member-name").text(data.email);
   // });
+
+  $('.dropdown-trigger').dropdown();
 });
