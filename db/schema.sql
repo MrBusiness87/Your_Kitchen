61a0f8d0ed13b63554f98a4d209aa02df8d76bb8
MrBusiness87 <bbartula87@gmail.com>
2020-08-29 13:15:43 -0400

    08/29/2020-LAST UPDATE

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,23 @@ CREATE DATABASE your_kitchenDB;
 
 USE your_kitchenDB;
 
-CREATE TABLE users_ingredients (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE users_ingredients
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
     ingredient_id INT,
-    ingredient_name VARCHAR(50),
+    ingredient_name VARCHAR
+    (50),
     ingredient_amount INT,
-    ingredient_unit VARCHAR(50)
+    ingredient_unit VARCHAR
+    (50)
 );
 
-CREATE TABLE favorite_recipes(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    CREATE TABLE favorite_recipes
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY,
     recipe_id INT NOT NULL,
-    recipe_name VARCHAR (50)
+    recipe_name VARCHAR
+        (50)
 );
